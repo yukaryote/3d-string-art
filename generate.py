2274fb169a19d30055505263e03eba3f412b3def
Isabella Yu <isabella.y.0303@gmail.com>
2024-05-14 08:27:55 -0400

    finalized pipeline

--- generate.py
+++ generate.py
@@ -8,83 +8,120 @@ import os
 import time
 
 
-def greedy(ref_images, frame, sampled_nails, num_iters):
+def greedy(frame, renderer, material, ref_images, view_params, samples_per_iter, num_iters):
     i = 0
     start_time = time.time()
+    best_nail = np.random.choice(len(frame.points), size=1).item()
+    nails_left = len(frame.points)
+    init_rendered_images = utils.render(renderer, view_params)
+    #cv.imshow("rendered string", init_renderered_images[0])
+    #cv.waitKey(0)  
+    prev_loss = np.mean((init_rendered_images - ref_images)**2)
+    print("Initial loss", prev_loss)
+    material = o3d.visualization.rendering.MaterialRecord()
+    utils.draw_line(frame, 0, 0)
+    material.line_width = 0.5
+    material.shader = "unlitLine"
+    material.base_color = [0, 0, 0, 1]  # Set color to black
+    renderer.scene.add_geometry("frame", frame, material)
+    rendered_images = utils.render(renderer, view_params)
+
     while i < num_iters:
-        ...
+        init_rendered_images = utils.render(renderer, view_params)
+        #cv.imshow(f"struct at iter {i}", init_rendered_images[0])
+        #cv.waitKey(1)   
+        prev_loss = np.mean((init_rendered_images - ref_images)**2)
+
+        print(f"Iteration {i} out of {num_iters}, {nails_left} nails left")
+        print(f"Loss: {prev_loss}")
+        sampled_indices = np.random.choice(len(frame.points), min(samples_per_iter, nails_left), replace=False)
+        
+        next_best_nail = best_nail
+        #print("best nail", best_nail)
+        for nail in sampled_indices:
+            #print("potential next nail", nail)
+            # Simulate the addition of this nail to the frame
+            utils.draw_line(frame, best_nail, nail)
+            renderer.scene.clear_geometry()
+            renderer.scene.add_geometry("frame", frame, material)
+            # o3d.visualization.draw_geometries([frame])            
+            # Render the frame to get the current image
+            rendered_images = utils.render(renderer, view_params)
+            #cv.imshow("rendered string", rendered_images[0])
+            #cv.waitKey(1)            
+            # Calculate the MSE loss with all reference images
+            current_loss = np.mean((rendered_images - ref_images) ** 2)
+            # Check if this is the best nail so far
+            if current_loss < prev_loss:
+                print("found improvement")
+                print("Loss", current_loss, prev_loss)
+                prev_loss = current_loss
+                next_best_nail = nail
+            
+            # Remove the line for the next iteration
+            utils.remove_line(frame, best_nail, nail)
+        
+        # After finding the best nail, add it permanently
+        print("found next best nail", best_nail, next_best_nail)
+        utils.draw_line(frame, best_nail, next_best_nail)
+        renderer.scene.clear_geometry()
+        renderer.scene.add_geometry("frame", frame, material)
+        nails_left -= 1
+        best_nail = next_best_nail
+        i += 1
+
     end_time = time.time()
     duration = end_time - start_time
     minutes = int(duration // 60)
     seconds = int(duration % 60)
     print(f"Algorithm took {minutes} minutes and {seconds} seconds to complete.")
+    return frame
 
-def conjugate_gradient(ref_images, frame, sampled_nails, num_iters):
+
+def conjugate_gradient(frame, renderer, material, ref_images, view_params, samples_per_iter, num_iters):
     pass
 
 
-def visualize(
-        images_fp: List[str],
-        mesh: str,
-        steps: int = 500,
-        num_points: int = 500,
-        ) -> o3d.geometry.LineSet:
-    images = utils.load_images(images_fp)
-    wf = utils.load_frame(mesh)
-    renderers = []
-
-    # TODO: add interactive stuff here to allow user to select their own camera views. Right now it's a hardcoded json
-    #save_view_point(wf, "camera_params/save_view_test_1.json", width=512, height=512)
-
-    for _ in range(len(images)):
-        # create new OfflineRenderer for each image
-        camera = o3d.io.read_pinhole_camera_parameters("camera_params/armadillo_512.json")
-        intrinsics, extrinsics = np.array(camera.intrinsic.intrinsic_matrix), np.array(camera.extrinsic)
-        width, height = camera.intrinsic.width, camera.intrinsic.height
-        # center, eye, up = extrinsics2lookat(np.array(camera.extrinsic)) LOL THEY DO TAKE IN EXTRINSICS NVM
-        renderer = o3d.visualization.rendering.OffscreenRenderer(width, height)
-        renderer.setup_camera(intrinsics, extrinsics, width, height)
-        #renderer.scene.add_geometry("wf", wf, mtl)
-        renderers.append(renderer)
-
-    # render the images
-    images = utils.render(renderers=renderers)
-    #print(images[0])
-    cv.imwrite("data/im.png", images[0])
-    print('finished writing')
-
-
-def main(frame_mesh_path: str, reference_path: str, num_points: int, num_iters: int, use_greedy: bool):
-    frame = utils.load_frame(frame_mesh_path)
-    ref_images = utils.generate_ref_images(reference_path)
+def main(frame_mesh_path: str, reference_path: str, samples_per_iter: int, num_iters: int, use_cg: bool, width: int = 512, height: int = 512):
+    print("frame path:", os.path.join(frame_mesh_path))
+    frame = utils.load_frame(os.path.join(frame_mesh_path))
+    material = o3d.visualization.rendering.MaterialRecord()
+    material.line_width = 0.5
+    material.shader = "unlitLine"
+    material.base_color = [0, 0, 0, 1]  # Set color to black
+    ref_images, view_params = utils.generate_ref_images(reference_path, num_views=8)
 
     # Sample num_points from the frame
     total_points = len(frame.points)
-    if num_points > total_points:
+    if samples_per_iter > total_points:
         raise ValueError("Number of points requested exceeds the number of available points in the frame.")
-    
-    sampled_indices = np.random.choice(total_points, num_points, replace=False)
-    sampled_points = np.array(frame.points)[sampled_indices]
 
-    if use_greedy:
+    strings_renderer = utils.create_renderer(frame, width=width, height=width)
+    rendered_frame = utils.render(strings_renderer, view_params)
+    #frame.lines = o3d.utility.Vector2iVector()
+    # cv.imshow("Rendered Frame init", rendered_frame[0])
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+    if not use_cg:
         # Placeholder for greedy algorithm function call
-        assert num_iters <= num_points, "Number of iterations must be less than or equal to num_points for greedy"
-        results = greedy(ref_images, frame, sampled_points, num_iters)
+        results = greedy(frame, strings_renderer, material, ref_images, view_params, samples_per_iter, num_iters)
     else:
         # Placeholder for conjugate gradient algorithm function call
-        results = conjugate_gradient(sampled_points, num_iters)
+        results = conjugate_gradient(num_iters)
 
     # Visualization or further processing can be done here with results
     print("Processing completed.")
+    o3d.visualization.draw_geometries([results])
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process mesh data.")
-    parser.add_argument("frame_mesh_path", type=str, default="data/sphere.py", help="Path to the frame mesh file.")
-    parser.add_argument("reference_mesh_path", type=str, default="data/bunny", help="Path to the reference mesh file.")
-    parser.add_argument("num_points", type=int, help="Number of points to sample from the frame mesh.")
-    parser.add_argument("num_iters", type=int, help="Number of iterations for the algorithm.")
-    parser.add_argument("--use_greedy", action='store_true', help="Use the greedy algorithm if set, otherwise use conjugate gradient.")
+    parser.add_argument("--frame_mesh_path", type=str, default="data/sphere.ply", help="Path to the frame mesh file.")
+    parser.add_argument("--reference_mesh_path", type=str, default="data/bunny", help="Path to the reference folder that contains the reference mesh.")
+    parser.add_argument("--num_iters", type=int, default=500, help="Number of iterations for the algorithm.")
+    parser.add_argument("--use_cg", action='store_true', help="Use the conjugate gradient algorithm if set, otherwise use greedy.")
 
     args = parser.parse_args()
+    samples_per_iter = int(args.num_iters / 4)
 
-    main(args.frame_mesh_path, args.reference_mesh_path, args.num_points, args.num_iters, args.use_greedy)
+    main(args.frame_mesh_path, args.reference_mesh_path, samples_per_iter, args.num_iters, args.use_cg)
--- utils.py
+++ utils.py
@@ -34,45 +34,74 @@ def load_mesh(fp: str):
     return mesh
 
 
-def generate_ref_images(fp: str, width: int = 512, height: int = 512, num_views: int = 5) -> np.array:
+def create_renderer(mesh, width: int = 512, height: int = 512, fov: int = 30.0,) -> o3d.visualization.rendering.OffscreenRenderer:
+    """Creates an OffscreenRenderer
+
+    Args:
+        mesh: _description_
+        width (int, optional): _description_. Defaults to 512.
+        height (int, optional): _description_. Defaults to 512.
+        fov (int, optional): _description_. Defaults to 30.0.
+
+    Returns:
+        o3d.visualization.rendering.OffscreenRenderer: renderer
+    """
+    # mesh = load_mesh(os.path.join(mesh_path))
+    renderer = o3d.visualization.rendering.OffscreenRenderer(width, height)
+    renderer.scene.set_background([1, 1, 1, 1])  # Set background to white
+    material = o3d.visualization.rendering.MaterialRecord()
+    if isinstance(mesh, o3d.geometry.LineSet):
+        print("Num points", len(mesh.points))
+        draw_line(mesh, 0, 0)
+        material.line_width = 0.5
+        material.shader = "unlitLine"
+    else:
+        mesh.compute_vertex_normals()
+        material.shader = "defaultUnlit"
+    material.base_color = [0, 0, 0, 1]  # Set color to black
+    renderer.scene.clear_geometry()
+    renderer.scene.add_geometry("mesh", mesh, material)
+    #renderer.scene.set_lighting(renderer.scene.LightingProfile.NO_SHADOWS, (0, 0, 0))
+    renderer.scene.set_lighting(renderer.scene.LightingProfile.MED_SHADOWS, (0.577, -0.577, -0.577))
+
+    aspect_ratio = width / height  # azimuth over elevation
+    near_plane = 0.1
+    far_plane = 50.0
+    fov_type = o3d.visualization.rendering.Camera.FovType.Vertical
+    renderer.scene.camera.set_projection(fov, aspect_ratio, near_plane, far_plane, fov_type)
+
+    print("created offscreen renderer")
+    return renderer
+
+
+def generate_ref_images(mesh_path: str, width: int = 512, height: int = 512, num_views: int = 5) -> np.array:
     """Generate reference images of a mesh as input to optimization. 
     The reference images are taken at uniform angles around the yaw axis of the object.
-    Two additional reference images are taken from the top and bottom.
-    So the total number of images outputted is num_views + 2.
 
     Args:
         fp (str): path to the reference mesh
         width, height (int, optional): width and height of the resulting image
         num_views (int, optional): number of reference images to generate from the yaw axis. Defaults to 5.
     """
-    mesh_name = fp.split("/")[-1]
-    mesh = load_mesh(os.path.join(fp, f"{mesh_name}.ply"))
+    fov = 30.0  # between 5 and 90 degrees
+    mesh_name = mesh_path.split("/")[-1]
+    mesh = load_mesh(os.path.join(mesh_path, f"{mesh_name}.ply"))
+    renderer = create_renderer(mesh, width=width, height=height, fov=fov)
+
+
     radius = 5.0
     eye = np.array([[radius, 0, radius]], dtype=np.float32).T  # Transposed to [3, 1]
     center = np.array([[0, 0, 0]], dtype=np.float32).T  # Transposed to [3, 1]
     up = np.array([[0, 1, 0]], dtype=np.float32).T  # Transposed to [3, 1]
-
-    # Configure the camera
-    renderer = o3d.visualization.rendering.OffscreenRenderer(width, height)
-    renderer.scene.set_background([1, 1, 1, 1])  # Set background to white
-    material = o3d.visualization.rendering.MaterialRecord()
-    material.base_color = [0, 0, 0, 1]  # Set color to black
-    renderer.scene.add_geometry("mesh", mesh, material)
-    renderer.scene.set_lighting(renderer.scene.LightingProfile.NO_SHADOWS, (0, 0, 0))
-    fov = 30.0  # between 5 and 90 degrees
-    aspect_ratio = width / height  # azimuth over elevation
-    near_plane = 0.1
-    far_plane = 50.0
-    fov_type = o3d.visualization.rendering.Camera.FovType.Vertical
-    renderer.scene.camera.set_projection(fov, aspect_ratio, near_plane, far_plane, fov_type)
-    print("created offscreen renderer")
-
     images = []
+    view_params = []
 
     for k in range(num_views):
         # render at angles 2pi * k / num_views around camera
         eye = np.array([[radius * np.cos(2 * np.pi * k / num_views), 0, radius * np.sin(2 * np.pi * k / num_views)]], dtype=np.float32).T
         renderer.scene.camera.look_at(center, eye, up)
+
+        view_params.append((center, eye, up))
 
         camera_params = o3d.camera.PinholeCameraParameters()
         fx = width / (2 * np.tan(np.deg2rad(fov / 2)))
@@ -86,19 +115,20 @@ def generate_ref_images(fp: str, width: int = 512, height: int = 512, num_views:
             cy=height / 2  
         )
         camera_params.extrinsic = np.linalg.inv(renderer.scene.camera.get_model_matrix())
-        output_file_path = os.path.join(fp, f"camera_params_view_{k}.json")
+        output_file_path = os.path.join(mesh_path, f"camera_params_view_{k}.json")
         o3d.io.write_pinhole_camera_parameters(output_file_path, camera_params)
-        
+
         img_o3d = renderer.render_to_image()
-        o3d.io.write_image(os.path.join(fp, f"ref_view_{k}.png"), img_o3d, 9)
+        o3d.io.write_image(os.path.join(mesh_path, f"ref_view_{k}.png"), img_o3d, 9)
         img = np.array(img_o3d)
+        img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
         images.append(img)
-        cv.imshow("model", img)
-        cv.waitKey(1)
-    return np.array(images)
+        #cv.imshow("model", img)
+        #cv.waitKey(1)
+    return np.array(images), view_params
 
 
-def load_frame(fp: str, num_points: int = 500) -> o3d.geometry.LineSet:
+def load_frame(fp: str, num_points: int = 250) -> o3d.geometry.LineSet:
     """Load a frame from filepath to connect strings to.
 
     Args:
@@ -114,7 +144,7 @@ def load_frame(fp: str, num_points: int = 500) -> o3d.geometry.LineSet:
     pcd = mesh.sample_points_uniformly(number_of_points=num_points)
     wireframe_pcd = o3d.geometry.LineSet()
     # convert point cloud to LineSet with no lines
-    wireframe_pcd.points = pcd.points
+    wireframe_pcd.points = o3d.utility.Vector3dVector(np.asarray(pcd.points))
     wireframe_pcd.lines = o3d.utility.Vector2iVector()
     return wireframe_pcd
 
@@ -131,7 +161,24 @@ def draw_line(wf: o3d.geometry.LineSet, i: int, j: int) -> None:
     wf.lines.append([i, j])
 
 
-def render(renderers: List[o3d.visualization.rendering.OffscreenRenderer]) -> np.array:
+def remove_line(wf: o3d.geometry.LineSet, i: int, j: int) -> None:
+    """Removes the line between points i and j, if it exists
+
+    Args:
+        wf (o3d.geometry.LineSet): LineSet wireframe
+        i (int): index of first point
+        j (int): index of second point
+    """
+    line = [i, j]
+    if line in wf.lines:
+        wf.lines.remove(line)
+    else:
+        line = [j, i]
+        if line in wf.lines:
+            wf.lines.remove(line)
+
+
+def render(renderer: o3d.visualization.rendering.OffscreenRenderer, view_params: List) -> np.array:
     """Renders the scene from specified cameras.
 
     Args:
@@ -144,18 +191,57 @@ def render(renderers: List[o3d.visualization.rendering.OffscreenRenderer]) -> np
     # TODO: please for the love of god vectorize this
 
     images = []
-    for r in renderers:
-        img = np.array(r.render_to_image())
+    for param in view_params:
+        center, eye, up = param
+        renderer.scene.camera.look_at(center, eye, up)
+        img_o3d = renderer.render_to_image()
+        img = np.array(img_o3d)
+        img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
         images.append(img)
+        # cv.imshow("render", img)
+        # cv.waitKey(100)
     return np.array(images)
 
 
-if __name__ == "__main__":
-    # wf = load_frame("data/sphere.ply")
+def dummy(frame, renderer):
+    material = o3d.visualization.rendering.MaterialRecord()
+    if isinstance(frame, o3d.geometry.LineSet):
+        print("Num points", len(frame.points))
+        draw_line(frame, 0, 0)
+        material.line_width = 0.5
+        material.shader = "unlitLine"
+    material.base_color = [0, 0, 0, 1]  # Set color to black
+    renderer.scene.clear_geometry()  # Clear previous geometries
+    renderer.scene.add_geometry("mesh", frame, material)
+    for i in range(0, len(frame.points)):
+        for j in range(i, len(frame.points)):
+            if np.random.rand() < 0.01:
+                draw_line(frame, i, j)
+    renderer.scene.add_geometry("modified_frame", frame, material)
+    rendered_images = render(renderer, view_params)
+    cv.imshow("TEST IN GREEDY", rendered_images[0])
+    cv.waitKey(1000)
 
-    # for i in range(0, len(wf.points)):
-    #     for j in range(i, len(wf.points)):
-    #         if np.random.rand() < 0.01:
-    #             draw_line(wf, i, j)
+
+if __name__ == "__main__":
+    wf = load_frame("data/sphere.ply")
+
+    for i in range(0, len(wf.points)):
+        for j in range(i, len(wf.points)):
+            if np.random.rand() < 0.01:
+                draw_line(wf, i, j)
     # o3d.visualization.draw_geometries([wf])
-    generate_ref_images("data/bunny")
+    images, view_params = generate_ref_images("data/sphere", num_views=5)
+    renderer = create_renderer(wf)
+    eye = np.array([[5, 0, 5]], dtype=np.float32).T  # Transposed to [3, 1]
+    center = np.array([[0, 0, 0]], dtype=np.float32).T  # Transposed to [3, 1]
+    up = np.array([[0, 1, 0]], dtype=np.float32).T  # Transposed to [3, 1]
+    images = render(renderer, view_params=view_params)
+    cv.imshow("TEST OUTSIDE GREEDY", images[0])
+    cv.waitKey(1000)
+
+    wf2 = load_frame("data/sphere.ply")
+    renderer2 = create_renderer(wf2)
+    dummy(frame=wf2, renderer=renderer2)
+
+    
